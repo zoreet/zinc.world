1a34384f694b91caa579191be7b474d0422ccaab
zoreet <zoreetdotcom@gmail.com>
2022-02-14 15:54:34 +0100

    update where the code is

--- js/main.js
+++ js/main.js
@@ -274,8 +274,8 @@ var $$ = {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/vladzinculescu/code/zinc.world/resources/js/main.js */"./resources/js/main.js");
-module.exports = __webpack_require__(/*! /Users/vladzinculescu/code/zinc.world/resources/sass/main.scss */"./resources/sass/main.scss");
+__webpack_require__(/*! /Users/zinc/Dropbox/work/web/world/zinc/zinc.world/resources/js/main.js */"./resources/js/main.js");
+module.exports = __webpack_require__(/*! /Users/zinc/Dropbox/work/web/world/zinc/zinc.world/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
