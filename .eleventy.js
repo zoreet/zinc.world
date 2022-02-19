37658825ccfbad84e5d83f8b41eaab2c5b5e75b8
zoreet <zoreetdotcom@gmail.com>
2022-02-19 12:16:41 +0100

    more fixes for sharing

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,14 @@ module.exports = function (eleventyConfig) {
     );
   });
 
+  eleventyConfig.addFilter("productionUrl", (uri) => {
+    return "https://zinc.world".concat(uri).replace(/\/\s*$/, "");
+  });
+
+  eleventyConfig.addFilter("encode", (url) => {
+    return encodeURIComponent(url);
+  });
+
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
   eleventyConfig.addFilter("htmlDateString", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
